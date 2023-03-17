d8c44cf89492059bd04e721ee8e7110051d08908
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2023-03-17 16:39:47 +0100

    implement jump func and turn char

--- Assets/Scripts/PlayerScripts/PlayerControle.cs
+++ Assets/Scripts/PlayerScripts/PlayerControle.cs
@@ -7,15 +7,25 @@ public class PlayerControle : MonoBehaviour
     [Header("Speed Settings")]
     [SerializeField][Tooltip("Vitesse de marche")] float _moveSpeed = 5f;
     [SerializeField][Tooltip("Vitesse de saut")] float _jumpForce = 10f;
+    [SerializeField][Tooltip("Nombre de saut Max")] int _jumpCountMax = 2;
+    [SerializeField][Tooltip("GroundPointCheck")] Transform _groundPoint;
+    [SerializeField][Tooltip("LayerMask WhatIsGround")] LayerMask _WhatIsGround;
+    
+    private int _jumpCount;
+    private bool _isOnGround;
 
 
     //Composants references
     [SerializeField] private Rigidbody2D _rigidbody;
     private Animator _animator;
     private PlayerInput _playerInput;
+    private Transform _transform;
+
+
 
     private void Awake()
     {
+        _transform = GetComponent<Transform>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();   
         _playerInput = GetComponent<PlayerInput>();
@@ -32,23 +42,46 @@ public class PlayerControle : MonoBehaviour
     public void DoWalk()
     {
         _rigidbody.velocity = new Vector2(_playerInput.Move * _moveSpeed, _rigidbody.velocity.y);
+        TurnChar();
     }
     #endregion
 
     #region TurneChar
     public void TurnChar()
     {
-
+        if(_playerInput.Move < 0)
+        {
+            _transform.GetChild(0).right = Vector2.left;
+        }
+        else if (_playerInput.Move > 0)
+        {
+            _transform.GetChild(0).right = Vector2.right;
+        }
     }
     #endregion
 
     #region DoJump
     public void DoJump()
     {
-
+        if (_jumpCount < _jumpCountMax && Input.GetButtonDown("Jump") )
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpForce);
+            _jumpCount++;
+        }
+        
     }
     #endregion
 
+
+   public void IsOnGround()
+   {
+       _isOnGround = Physics2D.OverlapCircle(_groundPoint.position, .2f, _WhatIsGround);
+        if (_isOnGround)
+        {
+            _jumpCount = 0;
+        }
+    }
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -58,7 +91,9 @@ public class PlayerControle : MonoBehaviour
     void Update()
     {
         DoWalk();
-        
+        DoJump();
+        IsOnGround();
+
     }
 
     #region
